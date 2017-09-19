ca420cfd26dede828d821796c4844a89cef70f70
Travis Crampton <crampton.travis@gmail.com>
2017-09-18 19:58:24 -0700

    add new

--- app/controllers/calculate_controller.rb
+++ app/controllers/calculate_controller.rb
@@ -1,6 +1,7 @@
 class CalculateController < ApplicationController
 
 	def generate
+		# wrap this in new service in large container obj
 		month = Months::CreateMonthDate.new(user: current_user).call
 		Months::GenerateMonth.new(month: month, previous_finance:current_user).create_finances_for_new_month
 		cash_flow = Months::CalculateCashFlow.new(month: month).call
