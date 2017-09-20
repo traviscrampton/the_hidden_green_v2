7d17b130bc9501677f9d0b8dd02a8d98fe8a3784
Travis Crampton <crampton.travis@gmail.com>
2017-09-20 14:06:54 -0700

    interest rates apply here

--- app/services/interest_rates/per_month.rb
+++ app/services/interest_rates/per_month.rb
@@ -1,7 +1,7 @@
 class InterestRates::PerMonth
 
 	attr_reader :amount, :interest_rate
-	
+
 	def initialize(args)
 		@amount = args[:amount]
 		@interest_rate = args[:interest_rate]
@@ -9,5 +9,7 @@ class InterestRates::PerMonth
 
 	def calculate
 		amount*(1 + (interest_rate/12))**(12)
+
+		#things to noodle => Should the thought be how much is being paid in interest and should that be persisted anywhere?
 	end
 end
